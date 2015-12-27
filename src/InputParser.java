3fa05698966d11c5c6dc7f7d44eeddbdc3bb81fa
Rupali Gangarde <rupalig@thoughtworks.com>
2015-12-27 16:32:49 +0530

    parsing input line

--- src/InputParser.java
+++ src/InputParser.java
@@ -2,8 +2,19 @@ public class InputParser {
     private Item item;
 
     public Item parseInput(String input) {
+        boolean isImported;
+        String productName;
+        String[] formattedInput = input.split(" ");
+        int quantity = Integer.parseInt(formattedInput[0]);
+        if(formattedInput[1]=="imported") {
+            isImported = true;
 
+        }
+        else{
+            isImported = false;
+        }
+
+        double productCost = Double.parseDouble(formattedInput[formattedInput.length-1]);
         return item;
-
     }
 }
